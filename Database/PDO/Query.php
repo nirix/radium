1f71bb7590033cebbf1b1fe1b061e06bff21275c
Jack Polgar <nrx@nirix.net>
2012-09-21 07:21:04 +1000

    Query builder rewrite started.

--- Database/PDO/Query.php
+++ Database/PDO/Query.php
@@ -1,32 +1,33 @@
 <?php
 /*!
- * Avalon
- * Copyright (C) 2011-2012 Jack Polgar
+ * Radium
+ * Copyright (C) 2011-2012 Jack P.
+ * https://github.com/nirix
  *
- * This file is part of Avalon.
+ * This file is part of Radium.
  *
- * Avalon is free software: you can redistribute it and/or modify
+ * Radium is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; version 3 only.
  *
- * Avalon is distributed in the hope that it will be useful,
+ * Radium is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Avalon. If not, see <http://www.gnu.org/licenses/>.
+ * along with Radium. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Avalon\Database\PDO;
+namespace Radium\Database\PDO;
 
-use Avalon\Database;
-use Avalon\Database\PDO;
+use Radium\Database;
+use Radium\Database\PDO;
 
 /**
  * PDO Database wrapper query builder
  *
- * @package Avalon
+ * @package Radium
  * @subpackage Database
  * @since 0.1
  * @author Jack P. <nrx@nirix.net>
@@ -34,359 +35,343 @@ use Avalon\Database\PDO;
  */
 class Query
 {
-	private $connection_name;
-	private $type;
-	private $cols;
-	private $table;
-	private $group_by = array();
-	private $where = array();
-	private $limit;
-	private $order_by = array();
-	private $custom_sql = array();
-	private $set;
-	private $_model;
+    /**
+     * The name of the connection to use.
+     *
+     * @var string
+     */
+    private $connectionName;
 
-	/**
-	 * PDO Query builder constructor.
-	 *
-	 * @param string $type
-	 * @param mixed $data
-	 *
-	 * @return object
-	 */
-	public function __construct($type, $data = null, $connection_name = 'main')
-	{
-		$this->connection_name = $connection_name;
+    /**
+     * The model to put the data into.
+     *
+     * @var string
+     */
+    private $model;
 
-		if ($type == 'SELECT') {
-			$this->cols = (is_array($data) ? $data : array('*'));
-		} else if ($type == 'INSERT INTO') {
-			$this->data = $data;
-		} else if ($type == 'UPDATE') {
-			$this->table = $data;
-		}
+    /**
+     * The query, joined together when needed.
+     *
+     * @var array
+     */
+    private $query = [];
 
-		$this->type = $type;
-		$this->prefix = $this->_conn()->prefix;
-		return $this;
-	}
+    /**
+     * Table prefix
+     *
+     * @var string
+     */
+    private $prefix;
 
-	/**
-	 * Enable use of the model object for table rows.
-	 *
-	 * @param string $model The model class.
-	 *
-	 * @return object
-	 */
-	public function _model($model)
-	{
-		$this->_model = $model;
-		return $this;
-	}
+    /**
+     * Values that need to be binded to the query.
+     *
+     * @var array
+     */
+    private $valuesToBind = [];
 
-	/**
-	 * Append DISTINCT to query type.
-	 *
-	 * @return object
-	 */
-	public function distinct()
-	{
-		$this->type = $this->type.' DISTINCT';
-		return $this;
-	}
+    /**
+     * PDO Query builder constructor.
+     *
+     * @param string $type
+     * @param mixed  $data
+     * @param string $connectionName
+     *
+     * @return object
+     */
+    public function __construct($type, $data = null, $connectionName = 'main')
+    {
+        // Set connection name
+        $this->connectionName = $connectionName;
 
-	/**
-	 * Set the table to select/delete from.
-	 *
-	 * @param string $table
-	 *
-	 * @return object
-	 */
-	public function from($table)
-	{
-		$this->table = $table;
-		return $this;
-	}
+        $this->query['type'] = $type;
 
-	/**
-	 * Set the table to insert data into.
-	 *
-	 * @param string $table
-	 *
-	 * @return object
-	 */
-	public function into($table)
-	{
-		$this->table = $table;
-		return $this;
-	}
+        // Figure out what to do with the
+        // $data parameter.
+        switch ($type) {
+            case "SELECT":
+            case "SELECT DISTINCT":
+                $this->query['select'] = ($data) ? $data : '*';
+                break;
 
-	/**
-	 * Sets the column => value data.
-	 *
-	 * @param array $data
-	 *
-	 * @return object
-	 */
-	public function set(array $data)
-	{
-		$this->data = $data;
-		return $this;
-	}
+            case "INSERT INTO":
+            case "UPDATE":
+                $this->query['table'] = $data;
+                break;
+        }
 
-	/**
-	 * Orders the query rows.
-	 *
-	 * @param string $col Column
-	 * @param string $dir Direction
-	 *
-	 * @return object
-	 */
-	public function order_by($col, $dir = 'ASC')
-	{
-		$this->order_by = array($col, $dir);
-		return $this;
-	}
+        // Set the prefix
+        //$this->prefix = $this->connection()->prefix;
 
-	/**
-	 * Insert custom SQL into the query.
-	 *
-	 * @param string $sql
-	 *
-	 * @return object
-	 */
-	public function custom_sql($sql)
-	{
-		$this->custom_sql[] = $sql;
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Easily add a "table = something" to the query.
-	 *
-	 * @example
-	 *    where("count", 5, ">=")
-	 *    or
-	 *    where(array(array('count', 5, '>=')));
-	 *
-	 * @param string $columm Column
-	 * @param mixed $value Column value
-	 * @param string $cond Condintional (=, !=, >=, <=, !=, etc)
-	 *
-	 * @return object
-	 */
-	public function where($columm, $value = null, $cond = '=')
-	{
-		// Check if this is a mass add
-		if (is_array($columm))
-		{
-			// Loop though the columns and add them
-			foreach($columm as $where)
-			{
-				$this->where($where[0], $where[1], (isset($where[2]) ? $where[2] : '='));
-			}
-		}
-		// Just one, add it.
-		else
-		{
-			$this->where[] = array($columm, $cond, $value === null ? 'NULL' : $value);
-		}
+    /**
+     * Enable use of the model object for table rows.
+     *
+     * @param string $model The model class.
+     *
+     * @return object
+     */
+    public function _model($model)
+    {
+        $this->_model = $model;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Set the table to select/delete from.
+     *
+     * @param string $table
+     *
+     * @return object
+     */
+    public function from($table)
+    {
+        $this->query['table'] = $table;
+        return $this;
+    }
 
-	/**
-	 * Limits the query rows.
-	 *
-	 * @param integer $from
-	 * @param integer $to
-	 *
-	 * @return object
-	 */
-	public function limit($from, $to = null)
-	{
-		$this->limit = implode(',', func_get_args());
-		return $this;
-	}
+    /**
+     * Set the table to insert data into.
+     *
+     * @param string $table
+     *
+     * @return object
+     */
+    public function into($table)
+    {
+        $this->query['table'] = $table;
+        return $this;
+    }
 
-	/**
-	 * Executes the query and return the statement.
-	 *
-	 * @return object
-	 */
-	public function exec()
-	{
-		$result = $this->_conn()->prepare($this->_assemble());
+    /**
+     * Sets the column => value data.
+     *
+     * @param array $data
+     *
+     * @return object
+     */
+    public function set(array $data)
+    {
+        $this->query['data'] = $data;
+        return $this;
+    }
 
-		if ($this->type != 'INSERT')
-		{
-			foreach ($this->where as $where)
-			{
-				$result->bind_value(':'. $where[0], $where[2]);
-			}
-		}
+    /**
+     * Orders the query rows.
+     *
+     * @param string $column Column
+     * @param string $how    Direction
+     *
+     * @return object
+     */
+    public function orderBy($column, $how = 'ASC')
+    {
+        if (!array_key_exists('order_by', $this->query)) {
+            $this->query['order_by'] = [];
+        }
 
-		return $result->_model($this->_model)->exec();
-	}
+        // Multiple?
+        if (is_array($column)) {
+            foreach ($column as $col => $how) {
+                $this->orderBy($col, $how);
+            }
+        } else {
+            $this->query['order_by'][] = str_replace('.', "`.`", "`{$column}` {$how}");
+        }
+        return $this;
+    }
 
-	/**
-	 * Private method used to compile the query into a string.
-	 *
-	 * @return string
-	 */
-	private function _assemble()
-	{
-		$query = array();
-		$query[] = $this->type;
+    /**
+     * Insert custom SQL into the query.
+     *
+     * @param string $sql
+     *
+     * @return object
+     */
+    public function sql($sql)
+    {
+        $this->query['sql'] = $sql;
+        return $this;
+    }
 
-		if (in_array($this->type, array("SELECT", "SELECT DISTINCT"))) {
-			$cols = array();
-			foreach ($this->cols as $col => $as)
-			{
-				// Check if we're fetching all columns
-				if ($as == '*')
-				{
-					$cols[] = '*';
-				}
-				// Check if we're fetching a column as an "alias"
-				else if (!is_numeric($col))
-				{
-					$cols[] = "`{$col}` AS `{$as}`";
-				}
-				// Normal column
-				else
-				{
-					$cols[] = "`{$as}`";
-				}
-			}
-			$query[] = implode(', ', $cols);
-		}
+    /**
+     * Easily add a "table = something" to the query.
+     *
+     * @example
+     *    where("count = ?", 5)
+     *    // or
+     *    where([["count = ?", 5], ["name LIKE 'Radium%'"]]);
+     *
+     * @param string $columm Column name
+     * @param mixed  $value  Column value
+     *
+     * @return object
+     */
+    public function where($column, $value = null)
+    {
+        if (!array_key_exists('where', $this->query)) {
+            $this->query['where'] = [];
+        }
 
-		// Select or Delete query
-		if (in_array($this->type, array("SELECT", "SELECT DISTINCT", "DELETE")))
-		{
-			$query[] = "FROM `{$this->prefix}{$this->table}`";
+        // Array? too easy
+        if (is_array($column)) {
+            $this->query['where'][] = $column;
+        } else {
+            $this->query['where'][] = [[$column, $value]];
+        }
 
-			// Where
-			$query = array_merge($query, $this->_build_where());
+        return $this;
+    }
 
-			// Group by
-			if (count($this->group_by) > 0)
-			{
-				$query[] = "GROUP BY " . implode(', ', $this->group_by);
-			}
+    /**
+     * An alias for where()
+     *
+     * @see Radium\Database\PDO\Query::where()
+     */
+    public function _or($column, $value = null)
+    {
+        $this->where($column, $value);
+        return $this;
+    }
 
-			// Custom SQL
-			if (count($this->custom_sql))
-			{
-				$query[] = implode(" ", $this->custom_sql);
-			}
+    /**
+     * Limits the query rows.
+     *
+     * @param integer $from
+     * @param integer $to
+     *
+     * @return object
+     */
+    public function limit($from, $to = null)
+    {
+        $this->query['limit'] = implode(',', func_get_args());
+        return $this;
+    }
 
-			// Order by
-			if (count($this->order_by) > 0)
-			{
-				$query[] = "ORDER BY `{$this->prefix}{$this->table}`.`{$this->order_by[0]}` {$this->order_by[1]}";
-			}
+    /**
+     * Executes the query and return the statement.
+     *
+     * @return object
+     */
+    public function exec()
+    {
+        $result = $this->connection()->prepare($this->_assemble());
 
-			// Limit
-			if ($this->limit != null)
-			{
-				$query[] = "LIMIT {$this->limit}";
-			}
-		}
-		// Insert query
-		else if($this->type == "INSERT INTO")
-		{
-			$query[] = "`{$this->prefix}{$this->table}`";
+        return $result->_model($this->_model)->exec();
+    }
 
-			$keys = array();
-			$values = array();
+    /**
+     * Private method used to compile the query into a string.
+     *
+     * @return string
+     */
+    public function assemble()
+    {
+        $queryString = [];
 
-			foreach($this->data as $key => $value) {
-				$keys[] = "`{$key}`";
-				$values[] = $this->_process_value($value);
-			}
+        $queryString[] = $this->query['type'];
 
-			$query[] = '(' . implode(', ', $keys) . ')';
-			$query[] = 'VALUES(' . implode(', ', $values) . ')';
-		}
-		// Update query
-		else if($this->type == "UPDATE")
-		{
-			$query[] = "`{$this->prefix}{$this->table}`";
+        // Select query
+        if ($this->query['type'] == "SELECT"
+        or  $this->query['type'] == "SELECT DISTINCT") {
+            // Build columns to select
+            $columns = [];
+            foreach ($this->query['select'] as $column => $as) {
+                // Normal column select
+                if (is_numeric($column)) {
+                    $columns[] = "`{$as}`";
+                }
+                // Alias
+                else {
+                    $columns[] = "`{$column}` AS `{$as}`";
+                }
+            }
 
-			$query[] = "SET";
-			$set = array();
-			foreach ($this->data as $column => $value)
-			{
-				$value = $this->_process_value($value);
-				$set[] = "`{$column}` = {$value}";
-			}
-			$query[] = implode(', ', $set);
+            // Join columns
+            $queryString[] = implode(', ', $columns);
+            unset($columns);
 
-			// Where
-			$query = array_merge($query, $this->_build_where());
-		}
+            // From
+            $queryString[] = "FROM `{$this->prefix}{$this->query['table']}`";
 
-		return implode(" ", $query);
-	}
+            // Where
+            $queryString[] = $this->buildWhere();
 
-	private function _build_where()
-	{
-		$query = array();
+            // Order by
+            $queryString[] = "ORDER BY " . implode(", ", $this->query['order_by']);
+        }
 
-		// Where
-		if (count($this->where))
-		{
-			$where = array();
+        return implode(" ", str_replace("%prefix%", $this->prefix, $queryString));
+    }
 
-			foreach ($this->where as $param)
-			{
-				$where[] = "`{$param[0]}` {$param[1]} :{$param[0]}";
-			}
+    private function buildWhere()
+    {
+        $query = [];
 
-			$query[] = "WHERE " . implode(' AND ', $where);
-			unset($where);
-		}
+        // Make sure there's something to do
+        if (count($this->query['where'])) {
+            foreach ($this->query['where'] as $group => $conditions) {
+                $group = []; // Yes, because the $group above is not used, get over it.
+                foreach ($conditions as $condition) {
+                    // Get column name
+                    $column = str_replace('`', '', explode(" ", $condition[0])[0]); // PHP 5.4 array dereferencing, fuck yeah!
 
-		return $query;
-	}
+                    $condition[0] = str_replace([$column, '.'], ["`{$column}`", "`.`"], $condition[0]);
 
-	/**
-	 * Processes the value.
-	 *
-	 * @return mixed
-	 */
-	private function _process_value($value)
-	{
-		if ($value === "NOW()") {
-			return $this->_conn()->quote(gmdate("Y-m-d H:i:s"));
-		} elseif ($value === "NULL") {
-			return 'NULL';
-		} else {
-			return $this->_conn()->quote($value);
-		}
-	}
+                    // Add value to the bind queue
+                    $this->valuesToBind[] = $condition[1];
+                    $valueBindKey = count($this->valuesToBind) - 1;
 
-	/**
-	 * Private function to return the database connection.
-	 *
-	 * @return object
-	 */
-	private function _conn()
-	{
-		return Database::connection($this->connection_name);
-	}
+                    // Add condition to group
+                    $group[] = str_replace("?", ":{$valueBindKey}_{$column}", $condition[0]);
+                }
 
-	/**
-	 * Magic method that converts the query to a string.
-	 * And some PHP team members have said PHP is not a magic language,
-	 * this is why Ruby is better.
-	 *
-	 * @return string
-	 */
-	public function __toString()
-	{
-		return $this->_assemble();
-	}
+                // Add the group
+                $query[] = "(" . implode(" AND ", $group) . ")";
+            }
+
+            // Return
+            return "WHERE " . implode(" OR ", $query);
+        }
+    }
+
+    /**
+     * Processes the value.
+     *
+     * @return mixed
+     */
+    private function processValue($value)
+    {
+        if ($value === "NOW()") {
+            return $this->connection()->quote(gmdate("Y-m-d H:i:s"));
+        } elseif ($value === "NULL") {
+            return 'NULL';
+        } else {
+            return $this->connection()->quote($value);
+        }
+    }
+
+    /**
+     * Private function to return the database connection.
+     *
+     * @return object
+     */
+    private function connection()
+    {
+        return Database::connection($this->connectionName);
+    }
+
+    /**
+     * Magic method that converts the query to a string.
+     * And some PHP team members have said PHP is not a magic language,
+     * this is why Ruby is better.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->assemble();
+    }
 }
