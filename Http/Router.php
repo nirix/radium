d42e51ccc3e733c400c7aea1287b7cc11fcc7b49
Jack Polgar <nrx@nirix.net>
2012-12-02 23:29:03 +1100

    Fixed issues with the Router.

--- Http/Router.php
+++ Http/Router.php
@@ -40,6 +40,7 @@ class Router
     public static $controller;
     public static $method;
     public static $params = [];
+    public static $vars = [];
 
     /**
      * Adds a route to be routed.
@@ -116,6 +117,7 @@ class Router
 
         static::$controller = str_replace('::', '\\', '\\'.$value[0]);
         static::$method = $method[0];
-        static::$params = (isset($method[1]) and $params = explode(',', $method[1])) ? $params : [];
+        static::$params = $route['params'];
+        static::$vars = (isset($method[1]) and $vars = explode(',', $method[1])) ? $vars : array();
     }
 }
